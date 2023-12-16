2ab02906ccaa5a990abbb62079b5ef9c6532baaf
Anton Kashnikov <anton.kashnikoff@icloud.com>
2023-12-16 15:23:23 +0100

    Initial Commit

---
+++ Project16/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Project16
+//
+//  Created by Антон Кашников on 16/12/2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
